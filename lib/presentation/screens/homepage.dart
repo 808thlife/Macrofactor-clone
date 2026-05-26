929d50f5ba72156dcb5977cdaeebbf4255f46467
808thlife <khqyop@gmail.com>
2026-05-26 12:46:51 +0500

    fixed import typo

--- lib/presentation/screens/homepage.dart
+++ lib/presentation/screens/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:macrofactor_clone/presentation/widgets/home_screen_dashboard.dart';
+import 'package:macrofactor_clone/presentation/widgets/home_dashboard/home_screen_dashboard.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
