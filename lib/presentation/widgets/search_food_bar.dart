cc6886d5208b5204075061d988475cd8aab54801
808thlife <khqyop@gmail.com>
2026-05-26 12:44:27 +0500

    added new widgets

---
+++ lib/presentation/widgets/search_food_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SearchFoodBar extends StatelessWidget{
+  const SearchFoodBar({super.key});
+   final late _formKey
+
+
+  @override
+  void init() {
+    final _formKey = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: Row(children: [FormField(key: _formKey, ,))],));
+  }
+}
